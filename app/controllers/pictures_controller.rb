24252b2e86c9c2b0871b8abeccb3e8564dee51ee
Colin Jones <colinsjones11@gmail.com>
2014-05-14 23:00:10 -0500

    Done w/ HW 7

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,45 @@
 class PicturesController < ApplicationController
+  def new
+
+  end
+
+  def create
+    @picture = Picture.new
+    @picture.caption = params[:caption]
+    @picture.source = params[:source]
+    @picture.save
+
+    redirect_to("/all_pictures")
+  end
+
+  def index
+    @all_pictures = Picture.all
+  end
+
+
   def show
+    @picture = Picture.find(params[:id])
+    @source = @picture.source
+    @caption = @picture.caption
+  end
+
+  def edit
+
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+    @picture.caption = params[:caption]
+    @picture.source = params[:source]
+    @picture.save
+
+    redirect_to("/picture_details/#{@picture.id}")
+
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id]).destroy
+
+    redirect_to("/all_pictures")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rxngif::Application.routes.draw do
+  ## C ##
+  get("/new_picture_form", { :controller => "pictures", :action => "new" })
+  get("/create_picture", { :controller => "pictures", :action => "create" })
+
+  ## R ##
   get("/picture_details/:id", { :controller => "pictures", :action => "show" })
+  get("/all_pictures", { :controller => "pictures", :action => "index" })
+
+  ## U ##
+  get("/edit_picture_form/:id", { :controller => "pictures", :action => "edit" })
+  get("/update_picture/:id", { :controller => "pictures", :action => "update" })
+
+  ## D ##
+  get("/delete_picture/:id", { :controller => "pictures", :action => "destroy" })
 end
